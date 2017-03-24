a8be9031caf31c7926675034161b8638e53b90d9
Keith Oppel <mistakeets@gmail.com>
2017-03-24 11:01:45 -0700

    adds example that creates a big letter K using the codebase (#5)
    * adds example that creates a big letter K using the codebase
    
    * remove console log from letter example code

---
+++ letterExample.js
@@ -0,0 +1,42 @@
+const letter = "K"
+
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+const whiteSpace = ' '
+const middleOfShape = alphabet.indexOf(letter) + 1
+
+let tempStorage = []
+for (let i = 0; i < middleOfShape; i++) {
+  tempStorage[i] = []
+}
+
+for (let i = 1; i < middleOfShape; i++) {
+  tempStorage[i].push(alphabet[i])
+}
+
+for (let i = 0; i < middleOfShape; i++) {
+  const spacesBeforeAndAfter = new Array((middleOfShape) - i).join(whiteSpace)
+  tempStorage[i].push(alphabet[i])
+  tempStorage[i].unshift(spacesBeforeAndAfter)
+  tempStorage[i].push(spacesBeforeAndAfter)
+  tempStorage[i].push(alphabet[i + 1])
+  tempStorage[i].push(alphabet[i + 2])
+  tempStorage[i].unshift(spacesBeforeAndAfter)
+}
+
+tempStorage[1].splice(2, 0, whiteSpace)
+for (let i = 2; i < middleOfShape; i++) {
+  const spacingMultiple = (3 + (i * 2) - 3)
+  const spacesBetweenLetters = new Array(spacingMultiple).join(whiteSpace)
+  tempStorage[i].splice(2, 0, spacesBetweenLetters)
+}
+
+let result = []
+for (let i = 0; i < middleOfShape; i++) {
+  const assembleLine = tempStorage[i].join('')
+  result.push(assembleLine)
+}
+for (let i = result.length - 2; i >= 0; i--) {
+  result.push(result[i])
+}
+
+console.log(result.join("\n") + "\n")
